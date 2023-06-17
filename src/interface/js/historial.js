dd17d7c3a6c3152724c1990fb9097a9d4aafb005
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-06-17 20:46:14 -0300

    feat(historial): nexo con datos precargados

--- src/interface/js/historial.js
+++ src/interface/js/historial.js
@@ -1,19 +1,6 @@
-import Plato from '../../domain/plato.js';
+import {resumen} from './data.js';
 
-const historialOrdenes = [
-  {
-    Plato: new Plato('Vegetariano', 'Pasta Primavera',
-        'Pasta saludable', '200', 'https://picsum.photos/200'),
-  },
-  {
-    Plato: new Plato('Regular', 'Hamburguesa de lentejas',
-        'Hamburguesa saludable', '150', 'https://picsum.photos/200'),
-  },
-  {
-    Plato: new Plato('Regular', 'Milanesa de pechuga premium',
-        'Milanesa al pan de pollo', '400', 'https://picsum.photos/200'),
-  },
-];
+const historialOrdenes = resumen.getOrdenes();
 
 function createHistorialHtml() {
   const historialOrdenesContainer =
@@ -21,35 +8,36 @@ function createHistorialHtml() {
   historialOrdenesContainer.innerHTML = '';
 
   historialOrdenes.forEach((orden) => {
-    const plato = orden.Plato;
-    const ordenContainer = document.createElement('div');
-    ordenContainer.classList.add('orden', 'card', 'bg-primary-light', 'mb-3');
+    orden.getPlatos().forEach((plato)=>{
+      const ordenContainer = document.createElement('div');
+      ordenContainer.classList.add('orden', 'card', 'bg-primary-light', 'mb-3');
 
-    const ordenBody = document.createElement('div');
-    ordenBody.classList.add('card-body', 'd-flex', 'flex-column');
+      const ordenBody = document.createElement('div');
+      ordenBody.classList.add('card-body', 'd-flex', 'flex-column');
 
-    const ordenPrecioFecha = document.createElement('div');
-    ordenPrecioFecha.classList.add('d-flex', 'justify-content-between');
+      const ordenPrecioFecha = document.createElement('div');
+      ordenPrecioFecha.classList.add('d-flex', 'justify-content-between');
 
-    const ordenPrecio = document.createElement('div');
-    ordenPrecio.classList.add('orden-precio');
-    ordenPrecio.textContent = `$${plato.getPrecio()}`;
+      const ordenPrecio = document.createElement('div');
+      ordenPrecio.classList.add('orden-precio');
+      ordenPrecio.textContent = `$${plato.getPrecio()}`;
 
-    const ordenFecha = document.createElement('p');
-    ordenFecha.classList.add('orden-fecha', 'm-0');
-    ordenFecha.textContent = `${plato.getDescripcion()}`;
+      const ordenFecha = document.createElement('p');
+      ordenFecha.classList.add('orden-fecha', 'm-0');
+      ordenFecha.textContent = `${plato.getDescripcion()}`;
 
-    const ordenTitulo = document.createElement('p');
-    ordenTitulo.classList.add('orden-titulo', 'm-0');
-    ordenTitulo.textContent = plato.getTitulo();
+      const ordenTitulo = document.createElement('p');
+      ordenTitulo.classList.add('orden-titulo', 'm-0');
+      ordenTitulo.textContent = plato.getTitulo();
 
-    ordenPrecioFecha.appendChild(ordenPrecio);
-    ordenPrecioFecha.appendChild(ordenFecha);
-    ordenBody.appendChild(ordenPrecioFecha);
-    ordenBody.appendChild(ordenTitulo);
+      ordenBody.appendChild(ordenTitulo);
+      ordenContainer.appendChild(ordenBody);
+      ordenPrecioFecha.appendChild(ordenFecha);
+      ordenPrecioFecha.appendChild(ordenPrecio);
+      ordenBody.appendChild(ordenPrecioFecha);
 
-    ordenContainer.appendChild(ordenBody);
-    historialOrdenesContainer.appendChild(ordenContainer);
+      historialOrdenesContainer.appendChild(ordenContainer);
+    });
   });
 }
 
