53e6410b724a2cdb1c7cd4cb27840558554742dd
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-06-18 19:36:59 -0300

    feat(reservas): add data platos reservados semana

--- src/interface/js/data.js
+++ src/interface/js/data.js
@@ -4,6 +4,7 @@ import Resumen from '../../domain/resumen.js';
 import Orden from '../../domain/orden.js';
 import Plato from '../../domain/plato.js';
 import Menu from '../../domain/menu.js';
+import Reserva from '../../domain/reserva.js';
 
 const alumno = 'Mariana Perez';
 
@@ -144,4 +145,84 @@ const ordenes = [orden1];
 resumen.setOrdenes(ordenes);
 resumen.setTotalGastos(ordenes[0].getTotal());
 
-export {alumno, tutor, comedor, resumen, menuSemanal, ordenes};
+const reservas = [
+  {Lunes: []},
+  {Martes: []},
+  {Miercoles: []},
+  {Jueves: []},
+  {Viernes: []},
+];
+
+// Reserva: platos, diaSemana, comedor, cantidad
+const reserva1 = new Reserva(
+    [platoRegLunes],
+    'Lunes',
+    comedor.getNombre(),
+    3,
+);
+reservas[0].Lunes.push(reserva1);
+const reserva2 = new Reserva(
+    [platoVegLunes],
+    'Lunes',
+    comedor.getNombre(),
+    7,
+);
+reservas[0].Lunes.push(reserva2);
+const reserva3 = new Reserva(
+    [platoVegMartes],
+    'Martes',
+    comedor.getNombre(),
+    5,
+);
+reservas[1].Martes.push(reserva3);
+const reserva4 = new Reserva(
+    [platoRegMartes],
+    'Martes',
+    comedor.getNombre(),
+    2,
+);
+reservas[1].Martes.push(reserva4);
+const reserva5 = new Reserva(
+    [platoRegMiercoles],
+    'Miercoles',
+    comedor.getNombre(),
+    1,
+);
+reservas[2].Miercoles.push(reserva5);
+const reserva6 = new Reserva(
+    [platoVegMiercoles],
+    'Miercoles',
+    comedor.getNombre(),
+    3,
+);
+reservas[2].Miercoles.push(reserva6);
+const reserva7 = new Reserva(
+    [platoRegJueves],
+    'Jueves',
+    comedor.getNombre(),
+    1,
+);
+reservas[3].Jueves.push(reserva7);
+const reserva8 = new Reserva(
+    [platoVegJueves],
+    'Jueves',
+    comedor.getNombre(),
+    2,
+);
+reservas[3].Jueves.push(reserva8);
+const reserva9 = new Reserva(
+    [platoRegViernes],
+    'Viernes',
+    comedor.getNombre(),
+    1,
+);
+reservas[4].Viernes.push(reserva9);
+const reserva10 = new Reserva(
+    [platoVegViernes],
+    'Viernes',
+    comedor.getNombre(),
+    0,
+);
+reservas[4].Viernes.push(reserva10);
+
+export {alumno, tutor, comedor, resumen, menuSemanal, ordenes, reservas};
